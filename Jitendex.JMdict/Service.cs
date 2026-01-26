51362967982ab2efaf24de4e97c4c08f250a323c
stephenmk <stephenmk@users.noreply.github.com>
2026-01-26 00:13:51 -0600

    Remove unnecessary using statement

--- Jitendex.JMdict/Service.cs
+++ Jitendex.JMdict/Service.cs
@@ -17,7 +17,6 @@ You should have received a copy of the GNU Affero General Public License along
 with Jitendex. If not, see <https://www.gnu.org/licenses/>.
 */
 
-using Microsoft.EntityFrameworkCore;
 using Jitendex.JMdict.Import.Models;
 using Jitendex.JMdict.Import.Parsing;
 using Jitendex.JMdict.Import.SQLite;
