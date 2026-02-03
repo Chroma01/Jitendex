7ca23a3d9dd7f0b9a506e5aed4bdb8a0e93b5d3e
stephenmk <stephenmk@users.noreply.github.com>
2026-02-02 23:28:41 -0600

    Add line breaks

--- Jitendex.JMdict/Import/Models/EntryElements/SubElements.cs
+++ Jitendex.JMdict/Import/Models/EntryElements/SubElements.cs
@@ -24,19 +24,58 @@ internal abstract record SubElement(int EntryId, int ParentOrder, int Order)
     public (int, int, int) Key() => (EntryId, ParentOrder, Order);
 }
 
-internal sealed record KanjiFormInfoElement(int EntryId, int ParentOrder, int Order, string TagName) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record KanjiFormPriorityElement(int EntryId, int ParentOrder, int Order, string TagName) : SubElement(EntryId, ParentOrder, Order);
+internal sealed record KanjiFormInfoElement
+    (int EntryId, int ParentOrder, int Order, string TagName)
+    : SubElement(EntryId, ParentOrder, Order);
 
-internal sealed record ReadingInfoElement(int EntryId, int ParentOrder, int Order, string TagName) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record ReadingPriorityElement(int EntryId, int ParentOrder, int Order, string TagName) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record RestrictionElement(int EntryId, int ParentOrder, int Order, string KanjiFormText) : SubElement(EntryId, ParentOrder, Order);
+internal sealed record KanjiFormPriorityElement
+    (int EntryId, int ParentOrder, int Order, string TagName)
+    : SubElement(EntryId, ParentOrder, Order);
 
-internal sealed record CrossReferenceElement(int EntryId, int ParentOrder, int Order, string TypeName, string RefText1, string? RefText2, int RefSenseNumber) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record DialectElement(int EntryId, int ParentOrder, int Order, string TagName) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record FieldElement(int EntryId, int ParentOrder, int Order, string TagName) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record GlossElement(int EntryId, int ParentOrder, int Order, string TypeName, string Text) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record KanjiFormRestrictionElement(int EntryId, int ParentOrder, int Order, string KanjiFormText) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record LanguageSourceElement(int EntryId, int ParentOrder, int Order, string? Text, string LanguageCode, string TypeName, bool IsWasei) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record MiscElement(int EntryId, int ParentOrder, int Order, string TagName) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record PartOfSpeechElement(int EntryId, int ParentOrder, int Order, string TagName) : SubElement(EntryId, ParentOrder, Order);
-internal sealed record ReadingRestrictionElement(int EntryId, int ParentOrder, int Order, string ReadingText) : SubElement(EntryId, ParentOrder, Order);
+internal sealed record ReadingInfoElement
+    (int EntryId, int ParentOrder, int Order, string TagName)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record ReadingPriorityElement
+    (int EntryId, int ParentOrder, int Order, string TagName)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record RestrictionElement
+    (int EntryId, int ParentOrder, int Order, string KanjiFormText)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record CrossReferenceElement
+    (int EntryId, int ParentOrder, int Order, string TypeName, string RefText1, string? RefText2, int RefSenseNumber)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record DialectElement
+    (int EntryId, int ParentOrder, int Order, string TagName)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record FieldElement
+    (int EntryId, int ParentOrder, int Order, string TagName)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record GlossElement
+    (int EntryId, int ParentOrder, int Order, string TypeName, string Text)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record KanjiFormRestrictionElement
+    (int EntryId, int ParentOrder, int Order, string KanjiFormText)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record LanguageSourceElement
+    (int EntryId, int ParentOrder, int Order, string? Text, string LanguageCode, string TypeName, bool IsWasei)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record MiscElement
+    (int EntryId, int ParentOrder, int Order, string TagName)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record PartOfSpeechElement
+    (int EntryId, int ParentOrder, int Order, string TagName)
+    : SubElement(EntryId, ParentOrder, Order);
+
+internal sealed record ReadingRestrictionElement
+    (int EntryId, int ParentOrder, int Order, string ReadingText)
+    : SubElement(EntryId, ParentOrder, Order);
